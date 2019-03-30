eba5fc5e7db7ce8cbb977ab1a845dcad5492395c
andrebnassis <andrebnassis@gmail.com>
2019-03-29 22:32:47 -0300

    Consolidate Newtosoft version between projects. Start to write unit test

--- JsonContentParser.UnitTests/JsonMapperTests.cs
+++ JsonContentParser.UnitTests/JsonMapperTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication1.Models;
+using ConsoleApplication1.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace JsonContentParser.UnitTests
 {
@@ -9,6 +13,19 @@ namespace JsonContentParser.UnitTests
         [TestMethod]
         public void WhenPassSomePath_ShouldReturnItsCorrespondantValue()
         {
+            var path18 = "example2[0].dictint.elementAt(0).Value()";
+
+            //Arrange
+            var JsonMapperService = new JsonMapper();
+            var example = new Example1(1, 1.0, new List<string> { "a", "b" }, new List<int> { 27, 1, 2, 3, 4, 5, 6, 50, 7, 8 }, new List<double> { 1.1, 1.2 }, new Dictionary<string, string> { { "asd", "123" }, { "def", "456" } }, new List<Example2> { new Example2(7, new Dictionary<string, string> { { "fgh", "789" } }, new Dictionary<int, string> { { 3, "dasdas" } }) });
+            var json = JsonConvert.SerializeObject(example);
+
+            //Act
+            var result = JsonMapperService.GetJsonProperty(json, path18);
+
+            //Assert
+            Assert.AreEqual("", result);
+
         }
     }
 }
